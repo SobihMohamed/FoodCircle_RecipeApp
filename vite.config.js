fa46f75bca7a9b77001587f7434fd38a43405aaa
Sobih <sobihmohamedsobih@gmail.com>
2024-10-19 15:18:16 +0300

    Fix base path and deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FoodCircle_RecipeApp",
+  base: "/FoodCircle_RecipeApp/",
   plugins: [react()],
 });
